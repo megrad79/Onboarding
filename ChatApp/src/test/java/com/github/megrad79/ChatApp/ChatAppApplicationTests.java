faf40b28314d0fd632e3f27c9c08890d05330723
megrad79 <megrad.79@gmail.com>
2021-09-28 23:14:47 -0400

    started ChatApp

---
+++ ChatApp/src/test/java/com/github/megrad79/ChatApp/ChatAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.megrad79.ChatApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
